892140da7956c2ad817294cb22f582950aedb8d5
Shyam Sathish <150350918+ShyamSathish005@users.noreply.github.com>
2026-02-05 10:46:22 +0530

    created the api for getting all the recipes, as well as searching the particular recipe

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from sqlalchemy import desc # Import this to handle descending order
 from database import get_db
 from models import Recipe
 from fastapi.middleware.cors import CORSMiddleware
@@ -8,12 +9,15 @@ app = FastAPI()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
+
 @app.get("/api/recipes")
 def read_recipes(page: int = 1, limit: int = 10, db: Session = Depends(get_db)):
-  
+   
     skip = (page - 1) * limit
     
-    query = db.query(Recipe)
+ 
+    query = db.query(Recipe).order_by(desc(Recipe.rating))
+    
     total = query.count()
     recipes = query.offset(skip).limit(limit).all()
     
@@ -23,11 +27,13 @@ def read_recipes(page: int = 1, limit: int = 10, db: Session = Depends(get_db)):
         "data": recipes 
     }
 
+
 @app.get("/api/recipes/search")
 def search_recipes(q: str, page: int = 1, limit: int = 10, db: Session = Depends(get_db)):
     skip = (page - 1) * limit
     
-    query = db.query(Recipe).filter(Recipe.title.ilike(f"%{q}%"))
+    query = db.query(Recipe).filter(Recipe.title.ilike(f"%{q}%")).order_by(desc(Recipe.rating))
+    
     total = query.count()
     recipes = query.offset(skip).limit(limit).all()
     
