088bd2c4f59be58b9bc065f9d7dbdccf3bed6cf8
Shyam Sathish <150350918+ShyamSathish005@users.noreply.github.com>
2026-02-05 10:31:41 +0530

    Created the sql lite database with proper database schema

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,24 @@
-from sqlalchemy import Column, Integer, JSON, String
+from sqlalchemy import Column, String, Integer, Float, JSON, Text
 from database import Base
-
 
 class Recipe(Base):
     __tablename__ = "recipes"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    ingredients = Column(JSON)
-    instructions = Column(JSON)
-    prep_time = Column(Integer, default=0)
+    
+
+    cuisine = Column(String)
+    title = Column(String)
+    serves = Column(String) 
+    
+
+    rating = Column(Float)
+    prep_time = Column(Integer)
+    cook_time = Column(Integer)
+    total_time = Column(Integer)
+    
+   
+    description = Column(Text)
+    
+   
+    nutrients = Column(JSON)
--- backend/sync.py
+++ backend/sync.py
@@ -3,31 +3,47 @@ import os
 from database import SessionLocal, engine
 import models
 
+models.Base.metadata.drop_all(bind=engine)
 models.Base.metadata.create_all(bind=engine)
 
 def sync_recipes():
     db = SessionLocal()
     
 
-    desktop_path = os.path.expanduser("/Users/shyamsathish/Desktop/US_recipes_null.json")
+    path = os.path.expanduser("/Users/shyamsathish/Desktop/US_recipes_null.json")
     
-    with open(desktop_path, 'r') as file:
-        recipes_data = json.load(file)
+    with open(path, 'r') as file:
+        data = json.load(file)
 
-        for r in recipes_data.values():
+        if isinstance(data, dict):
+            items = data.values()
+        elif isinstance(data, list):
+            items = data
+        else:
+            items = []
+
+        for item in items:
+            if not isinstance(item, dict):
+                continue
 
             new_recipe = models.Recipe(
-                name=r.get('title'),
-                ingredients=r.get('ingredients'),
-                instructions=r.get('instructions'),
-                prep_time=r.get('prep_time', 0)
+                cuisine=item.get('cuisine'),
+                title=item.get('title'),
+                rating=item.get('rating'),
+                prep_time=item.get('prep_time'),
+                cook_time=item.get('cook_time'),
+                total_time=item.get('total_time'),
+                description=item.get('description'),
+                nutrients=item.get('nutrients'),
+                serves=item.get('serves')
             )
-            
+        
             db.add(new_recipe)
             
+
     db.commit()
     db.close()
-    print("Recipes synced successfully!")
+
 
 if __name__ == "__main__":
     sync_recipes()
